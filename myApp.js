a7380e3f26846e1f9a63bc5bd9b5b3e59edf3f1d
Elliot Voris <elliot@voris.me>
2021-07-14 12:04:32 -0500

    Serve an HTML File
    Used the `options` argument to define a root directory.
    
    Note: Changing that root directory seems to require npm to stop and restart
    for the changes to take effect.
    
    Signed-off-by: Elliot Voris <elliot@voris.me>

--- myApp.js
+++ myApp.js
@@ -2,16 +2,20 @@ var express = require('express');
 var app = express();
 
 // 1. Meet the Node Console
-console.log("Hello World")
+// console.log("Hello World")
 
 // 2. Start a Working Express Server
+// app.get('/', (req, res) => {
+//   res.send('Hello Express')
+// })
+
+// 3. Serve an HTML File
+const options = {
+  root: __dirname,
+}
 app.get('/', (req, res) => {
-  res.send('Hello Express')
+  res.sendFile("views/index.html", options)
 })
-
-
-
-
 
 
 
