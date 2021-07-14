0f60f2383e76a6f3013477e4f780039b3e0e00fe
Elliot Voris <elliot@voris.me>
2021-07-14 09:53:26 -0500

    Start a Working Express Server
    Now that I figured out the structure for this particular course, we're
    off to the races!
    
    Signed-off-by: Elliot Voris <elliot@voris.me>

--- myApp.js
+++ myApp.js
@@ -4,8 +4,10 @@ var app = express();
 // 1. Meet the Node Console
 console.log("Hello World")
 
-
-
+// 2. Start a Working Express Server
+app.get('/', (req, res) => {
+  res.send('Hello Express')
+})
 
 
 
